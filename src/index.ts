f7aadd67fba27f31da8b30e64f8c69ef391dfb39
Lakhte Ali Agha <lagha@ltvco.com>
2022-08-08 00:17:56 -0400

    setup mikroorm config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [],
-    dbName: "lireddit",
-    type: "postgresql",
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(mikroOrmConfig);
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
 };
 
 main();
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,16 @@
+import { Options } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import path from "path";
+
+const config: Options = {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    glob: "!(*.d).{js,ts}",
+  },
+  entities: [Post],
+  dbName: "lireddit",
+  type: "postgresql",
+  debug: !__prod__,
+};
+export default config;
