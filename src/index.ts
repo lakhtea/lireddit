c4539f30ee82cd4435fea5faa62f9837d1e1094e
Lakhte Ali Agha <lagha@ltvco.com>
2022-08-07 23:52:33 -0400

    setup mikroorm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [],
+    dbName: "lireddit",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+};
+
+main();
