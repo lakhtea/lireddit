64e300144d186cf92f1fab882eaec542813f32fb
Lakhte Ali Agha <lagha@ltvco.com>
2022-08-22 22:24:09 -0400

    added isAuth

---
+++ src/middleware/isAuth.ts
@@ -0,0 +1,8 @@
+import { MyContext } from "../types";
+import { MiddlewareFn } from "type-graphql";
+
+export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
+  if (!context.req.session.userId) throw new Error("no user logged in");
+
+  return next();
+};
